3e8d025c4544b5c31b697e845f892445bba3a774
TetianaYehorova23 <tetyanaego34@gmail.com>
2023-05-15 10:54:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
+    const messageElement = formElement.querySelector(".form_message");
 
     messageElement.textContent = message;
-    messageElement.classList.remove("form__message--success", "form__message--error");
-    messageElement.classList.add(`form__message--${type}`);
+    messageElement.classList.remove("form_message-success", "form_message-error");
+    messageElement.classList.add(`form_message-${type}`);
 }
 
 function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+    inputElement.classList.add("form_input-error");
+    inputElement.parentElement.querySelector(".form_input-error-message").textContent = message;
 }
 
 function clearInputError(inputElement) {
-    inputElement.classList.remove("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+    inputElement.classList.remove("form_input-error");
+    inputElement.parentElement.querySelector(".form_input-error-message").textContent = "";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -22,14 +22,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelector("#linkCreateAccount").addEventListener("click", e => {
         e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
+        loginForm.classList.add("form-hidden");
+        createAccountForm.classList.remove("form-hidden");
     });
 
     document.querySelector("#linkLogin").addEventListener("click", e => {
         e.preventDefault();
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
+        loginForm.classList.remove("form-hidden");
+        createAccountForm.classList.add("form-hidden");
     });
 
     loginForm.addEventListener("submit", e => {
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
         setFormMessage(loginForm, "error", "Invalid username/password combination");
     });
 
-    document.querySelectorAll(".form__input").forEach(inputElement => {
+    document.querySelectorAll(".form_input").forEach(inputElement => {
         inputElement.addEventListener("blur", e => {
             if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
                 setInputError(inputElement, "Username must be at least 10 characters in length");
